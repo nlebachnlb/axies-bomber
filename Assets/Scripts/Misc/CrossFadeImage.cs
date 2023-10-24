b35236a859be641e277791eba191b169e56bbf9f
Bach Nguyen <nlebachnlb.develop@gmail.com>
2023-10-25 01:05:00 +0700

    [Feat] Home UI

---
+++ Assets/Scripts/Misc/CrossFadeImage.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CrossFadeImage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Image image;
+
+    private void Start()
+    {
+        CrossFade(0f, 0f);
+    }
+
+    public void CrossFade(float endValue, float duration)
+    {
+        image.CrossFadeAlpha(endValue, duration, true);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        CrossFade(0.4f, 0.15f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        CrossFade(0f, 0.15f);
+    }
+}
