26b2bfe660f82ff33631fce4c17c580d8f03f14e
Nam Mai <mdnam2410@gmail.com>
2024-06-25 23:06:08 +0700

    [Feat] Ironclad Barrier

--- Assets/Scripts/Player/Ability/IroncladBarrier.cs
+++ Assets/Scripts/Player/Ability/IroncladBarrier.cs
@@ -8,9 +8,17 @@ public class IroncladBarrier : AxieAbility<IroncladBarrierStats>
     [SerializeField] private IroncladBarrierStats defaultStats;
     [SerializeField] private Cooldown cooldown;
 
+    private JumpController jumpController;
+
     private void Awake()
     {
         Stats = Instantiate(defaultStats);
+    }
+
+    public override void AssignOwner(GameObject owner)
+    {
+        base.AssignOwner(owner);
+        jumpController = owner.GetComponent<JumpController>();
     }
 
     public override bool CanDeploy()
@@ -21,5 +29,23 @@ public class IroncladBarrier : AxieAbility<IroncladBarrierStats>
     public override void DeployAbility()
     {
         cooldown.StartCountdown();
+        jumpController.Jump(Owner.transform.position, 1, 0.25f, onCompleted: PerformPush);
+    }
+
+    private void PerformPush()
+    {
+        var origin = Owner.transform.position;
+        var results = Physics.OverlapSphere(origin, Stats.EffectRadius);
+        if (results != null && results.Length > 0)
+        {
+            foreach (var result in results)
+            {
+                if (result.transform.CompareTag(Tag.ENEMY) && result.TryGetComponent<EnemyController>(out var enemyController))
+                {
+                    var force = (result.transform.position - origin).normalized * 10;
+                    enemyController.AddForce(force, 2f);
+                }
+            }
+        }
     }
 }
